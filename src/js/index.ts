e9fc73f5a2a05964e6fe5828b2c0a55cf47d5c5e
Justin Macdonald Hansen <just1392@edu.easj.dk>
2020-10-13 16:17:33 +0200

    Done assignment 2, I know I should have done smaller commits, but I got carried away and finished it all in one go. I added to upper case, to lower case and to pig latin string conversions on the entered string

--- src/js/index.ts
+++ src/js/index.ts
@@ -1,12 +1,73 @@
-interface Person {
-    firstName: string;
-    lastName: string;
+let inputField = document.getElementById("stringInput");
+let resultsTable: HTMLTableElement = <HTMLTableElement> document.getElementById("resultTable");
+let idCount: number = 0;
+
+document.getElementById("newStringBtn").addEventListener("click", addString);
+
+function addString(){
+    let newRow: HTMLTableRowElement = <HTMLTableRowElement> resultsTable.insertRow(-1);
+    let id: HTMLTableCellElement = <HTMLTableCellElement> newRow.insertCell(0);
+    let originalString: HTMLTableCellElement = <HTMLTableCellElement> newRow.insertCell(1);
+    let newString: HTMLTableCellElement = <HTMLTableCellElement> newRow.insertCell(2);
+    let inputText = (<HTMLInputElement>inputField).value;
+
+    let selector: HTMLSelectElement = <HTMLSelectElement> document.getElementById("stringManipulation");
+    let option: HTMLOptionElement = selector.options[selector.selectedIndex];
+
+    id.setAttribute("id", "id: " + idCount.toString());
+    originalString.setAttribute("id", "os: " + idCount.toString());
+    newString.setAttribute("id", "ns: " + idCount.toString());
+    
+    id.innerHTML = idCount.toString();
+    originalString.innerHTML = inputText;
+
+    if (option.getAttribute("id") == "toUpperCase"){
+        newString.innerHTML = ConvertToUpper(inputText);
+    }
+    if (option.getAttribute("id") == "toLowerCase"){
+        newString.innerHTML = ConvertToLower(inputText);
+    }    
+    if (option.getAttribute("id") == "toPigLatin"){
+        newString.innerHTML = ConvertToPigLatin(inputText);
+    }
+    
+    idCount++;
 }
 
-function greeter(person: Person): string {
-    return "Hello, " + person.firstName + " " + person.lastName;
+function ConvertToUpper(stringToConvert: string): string{
+    stringToConvert = stringToConvert.toUpperCase();
+    return stringToConvert;
 }
-let user: Person = { firstName: "John", lastName: "Doe" };
 
-let element: HTMLDivElement = <HTMLDivElement> document.getElementById("content");
-element.innerHTML = greeter(user);
+function ConvertToLower(stringToConvert: string): string{
+    stringToConvert = stringToConvert.toLowerCase();
+    return stringToConvert;
+}
+
+function ConvertToPigLatin(s: string): string{
+    s = s.toLowerCase();
+    let resultString: string = "";
+    let temp: string = "";
+    let vowel = ['a', 'e', 'i', 'o', 'u'] 
+    let consonantStart: boolean  = false;;
+
+    for (let index = 0; index < s.length; index++) {
+        let findChar = vowel.some(c => c.toLowerCase() == s[index]);
+        if (findChar){
+            break;
+        }
+        else {
+            consonantStart = true;
+            temp += s[index];
+            resultString = s.substring(index + 1, s.length);
+        }
+    }
+    if (consonantStart){
+        resultString += temp + "ay";
+    }
+    else {
+        resultString += s + "yay";
+    }
+
+    return resultString;
+}
